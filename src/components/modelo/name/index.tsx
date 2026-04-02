9b2fb633f042cbc028eaf2b4e8f6cbcc599414bb
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-02 10:59:21 -0300

    Criação modelo de pasta

---
+++ src/components/modelo/name/index.tsx
@@ -0,0 +1,5 @@
+import { NameContainer } from './styles'
+
+export function name() {
+  <NameContainer/>
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,101 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  .title-xl {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 3rem; /* 48px */
+    line-height: 130%;
+    font-weight: 800;
+  }
+
+  .title-l {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem; /* 32px */
+    line-height: 130%;
+    font-weight: 800;
+  }
+
+  .title-m {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.5rem; /* 24px */
+    line-height: 130%;
+    font-weight: 800;
+  }
+
+  .title-s {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.25rem; /* 20px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .title-xs {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.125rem; /* 18px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .text-l {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.25rem; /* 20px */
+    line-height: 130%;
+    font-weight: 400;
+  }
+
+  .text-l-bold {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.25rem; /* 20px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .text-m {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem; /* 16px */
+    line-height: 130%;
+    font-weight: 400;
+  }
+
+  .text-m-bold {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem; /* 16px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .text-s {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.875rem; /* 14px */
+    line-height: 130%;
+    font-weight: 400;
+  }
+
+  .text-xs {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.75rem; /* 12px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .tag {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.625rem; /* 10px */
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  .button-g {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.875rem; /* 14px */
+    line-height: 160%;
+    font-weight: 700;
+  }
+
+  .button-m {
+    font-family: 'Roboto', sans-serif;
+    font-size: 0.75rem; /* 12px */
+    line-height: 160%;
+    font-weight: 400;
+  }
+`
