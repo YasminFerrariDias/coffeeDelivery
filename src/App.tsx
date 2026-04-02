c45cd37ef97713b92024ae0455fc4e37704de2f6
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-02 16:52:07 -0300

    Alteração no Componente Botão - Acrescente tipos de classname

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,36 @@ import { Text } from './components/Text'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Title variant='title-xl' text='Título XL' />
-      <br />
-      <Title variant='title-l' text='Título L' />
-      <br />
-      <Title variant='title-m' text='Título M' />
-      <br />
-      <Title variant='title-s' text='Título S' />
-      <br />
-      <Title variant='title-xs' text='Título XS' />
-      <br />
-      <Text variant='text-l' text='Texto L'/>
-      <br />
-      <Text variant='text-l-bold' text='Texto L - BOLD'/>
-      <br />
-      <Text variant='text-m' text='Texto M'/>
-      <br />
-      <Text variant='text-m-bold' text='Texto M - BOLD'/>
-      <br />
-      <Text variant='text-s' text='Texto S'/>
-      <br />
-      <Text variant='text-xs' text='Texto XS'/>
-      <br />
-   
+      <div className='Title'>
+        <Title variant='title-xl' text='Título XL' />
+        <br />
+        <Title variant='title-l' text='Título L' />
+        <br />
+        <Title variant='title-m' text='Título M' />
+        <br />
+        <Title variant='title-s' text='Título S' />
+        <br />
+        <Title variant='title-xs' text='Título XS' />
+      </div>
 
-      <Button icon='MapPin' text='Porto Alegre, RS' variant='purple'/>
-      <Button icon='ShoppingCart' variant='yellow'/>
+      <div className='Text'>
+        <Text variant='text-l' text='Texto L' />
+        <br />
+        <Text variant='text-l-bold' text='Texto L - BOLD' />
+        <br />
+        <Text variant='text-m' text='Texto M' />
+        <br />
+        <Text variant='text-m-bold' text='Texto M - BOLD' />
+        <br />
+        <Text variant='text-s' text='Texto S' />
+        <br />
+        <Text variant='text-xs' text='Texto XS' />
+      </div>
+
+      <div className='Button'>
+        <Button icon='MapPin' text='Porto Alegre, RS' variant='purple' className='text-s' />
+        <Button icon='ShoppingCart' variant='yellow' />
+      </div>
 
       <GlobalStyle />
     </ThemeProvider>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,16 +5,17 @@ interface ButtonProps {
   icon: 'ShoppingCart' | 'MapPin',
   text?: string,
   variant: 'yellow' | 'purple'
+  className?: 'tag' | 'button-g' | 'button-m' | 'text-s'
 }
 
-export function Button({ icon, text, variant }: ButtonProps) {
+export function Button({ icon, text, variant, className }: ButtonProps) {
   const Icon = {
     ShoppingCart,
     MapPin,
   }[icon]
 
   return (
-    <ButtonContainer variant={variant} className='text-s'>
+    <ButtonContainer variant={variant} className={className}>
       <Icon size={22} />
       {text && <span>{text}</span>}
     </ButtonContainer>
