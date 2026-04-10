09a3ecb03d5ff885d3064ca269e7fb579e4910d1
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-10 16:36:52 -0300

    Criação da página Checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,14 +1,30 @@
 import { CartCard } from "../../components/CartCard";
-
-{/*import { AddressCard } from "../../components/AddressCard";
-import { PaymentCard } from "../../components/PaymentCard";*/}
+import { AddressCard } from "../../components/AddressCard";
+import { PaymentCard } from "../../components/PaymentCard";
+import { Header } from "../../components/Header";
+import { CheckoutContainer, CompleteYourOrder, Container, SelectedCoffees } from "./styles";
+import { Title } from "../../components/Title";
 
 export function Checkout() {
   return (
     <>
-{/*}      <AddressCard />
-      <PaymentCard />*/}
-      <CartCard />
+      <Header />
+      <CheckoutContainer>
+        <CompleteYourOrder>
+          <Title text="Complete seu pedido" $color="base-subtitle" $variant="title-xs" />
+
+          <Container>
+            <AddressCard />
+            <PaymentCard />
+          </Container>
+        </CompleteYourOrder>
+
+        <SelectedCoffees>
+          <Title text="Cafés selecionados" $color="base-subtitle" $variant="title-xs" />
+
+          <CartCard />
+        </SelectedCoffees>
+      </CheckoutContainer>
     </>
   )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const CheckoutContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+export const CompleteYourOrder = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 2.3rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const SelectedCoffees = styled.div`
+  margin-top: 2.3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
