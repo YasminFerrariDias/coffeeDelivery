aa8ccfea258d36b17c18698d981d4c8a98c38cbf
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-10 16:13:21 -0300

    Criação do componente CartCard

--- src/components/CartCard/index.tsx
+++ src/components/CartCard/index.tsx
@@ -1,10 +1,36 @@
+import { Card } from "../Card";
 import { CoffeeCartCard } from "../CoffeeCartCard";
-import { CartCardContainer } from "./styles";
+import { Price } from "../Price";
+import { Text } from "../Text";
+import { Button, CartCardContainer, Row, Separate, TotalInformation } from "./styles";
 
 export function CartCard() {
   return (
-    <CartCardContainer>
-      <CoffeeCartCard img="ExpressoTradicional" text="Expresso Tradicional" number={9.90} />
-    </CartCardContainer>
+    <Card $variant="mediumRounded">
+      <CartCardContainer>
+        <CoffeeCartCard img="ExpressoTradicional" text="Expresso Tradicional" number={9.90} />
+        <Separate />
+        <CoffeeCartCard img="Latte" text="Latte" number={9.90} />
+        <Separate />
+
+        <TotalInformation>
+          <Row>
+            <Text text="Total de itens" $variant="text-s" color="base-text" />
+            <Price $number={29.70} $variant="smallPrice" />
+          </Row>
+
+          <Row>
+            <Text text="Entrega" $variant="text-s" color="base-text" />
+            <Price $number={3.50} $variant="smallPrice" />
+          </Row>
+
+          <Row>
+            <Text text="Total" $variant="text-l-bold" color="base-subtitle" />
+            <Price $number={33.20} $variant="largePrice" />
+          </Row>
+        </TotalInformation>
+        <Button>CONFIRMA PEDIDO</Button>
+      </CartCardContainer>
+    </Card>
   )
 }
--- src/components/CartCard/styles.ts
+++ src/components/CartCard/styles.ts
@@ -1,5 +1,37 @@
 import styled from "styled-components";
 
 export const CartCardContainer = styled.div`
-
+  padding: 2rem;
 `;
+
+export const Separate = styled.div`
+  height: 1px;
+  width: auto;
+  background-color: ${(props) => props.theme["base-button"]};
+  margin: 1rem 0;
+`;
+
+export const TotalInformation = styled.div`
+  margin: 0.75rem 0;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Row = styled.div`
+  display: flex;
+  margin: 0.75rem 0;
+  flex-direction: row;
+  justify-content: space-between
+`
+
+export const Button = styled.div`
+  background-color: ${(props) => props.theme['yellow']};
+  color: ${(props) => props.theme['white']};
+  width: auto;
+  height: 2.875rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  border-radius: 0.375rem;
+`
+
