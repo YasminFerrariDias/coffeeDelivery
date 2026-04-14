c283b915a0015c8e99728f36b8dccbd1b238c18a
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-14 10:44:04 -0300

    Criando e aplicando o json de img

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -4,23 +4,11 @@ import { Text } from "../Text";
 import { Price } from "../Price";
 import { Count } from "../Count"; 
 import { Button } from "../Button";
-
-import Americano from '../../assets/img/Type=Americano.svg';
-import Arabe from '../../assets/img/Type=Árabe.svg';
-import CafeComLeite from '../../assets/img/Type=CafécomLeite.svg';
-import CafeGelado from '../../assets/img/Type=CaféGelado.svg';
-import Capuccino from '../../assets/img/Type=Capuccino.svg';
-import ChocolateQuente from '../../assets/img/Type=ChocolateQuente.svg';
-import Cubano from '../../assets/img/Type=Cubano.svg';
-import ExpressoCremoso from '../../assets/img/Type=ExpressoCremoso.svg';
-import Expresso from '../../assets/img/Type=Expresso.svg';
-import Havaiano from '../../assets/img/Type=Havaiano.svg';
-import Irlandes from '../../assets/img/Type=Irlandês.svg';
-import Latte from '../../assets/img/Type=Latte.svg';
-import Macchiato from '../../assets/img/Type=Macchiato.svg';
-import Mochaccino from '../../assets/img/Type=Mochaccino.svg';
 import { Card } from "../Card";
 import { Tag } from "../Tag";
+import { coffeeImages } from '../../consts/coffeeImages'
+
+type CoffeeImage = keyof typeof coffeeImages
 
 type TagOption =
   | 'TRADICIONAL'
@@ -34,27 +22,11 @@ interface CoffeeCardProps {
   text: string
   textTitle: string
   price: number
-  img: 'Americano'
-  | 'Arabe'
-  | 'CafeComLeite'
-  | 'CafeGelado'
-  | 'Capuccino'
-  | 'ChocolateQuente'
-  | 'Cubano'
-  | 'ExpressoCremoso'
-  | 'Expresso'
-  | 'Havaiano'
-  | 'Irlandes'
-  | 'Latte'
-  | 'Macchiato'
-  | 'Mochaccino'
+  img: CoffeeImage
 }
 
 export function CoffeeCard({ img, tagValue, textTitle, text, price }: CoffeeCardProps) {
-  const Img = {
-    Americano, Arabe, CafeComLeite, CafeGelado, Capuccino, ChocolateQuente, Cubano, ExpressoCremoso, Expresso, Havaiano, Irlandes, Latte, Macchiato, Mochaccino,
-  }[img]
-
+  const Img = coffeeImages[img]
   return (
     <Card $variant="smallRounded"  >
       <CoffeeCardContainer>
---
+++ src/consts/coffeeImages.ts
@@ -0,0 +1,31 @@
+import Americano from '../assets/img/Type=Americano.svg';
+import Arabe from '../assets/img/Type=Árabe.svg';
+import CafeComLeite from '../assets/img/Type=CafécomLeite.svg';
+import CafeGelado from '../assets/img/Type=CaféGelado.svg';
+import Capuccino from '../assets/img/Type=Capuccino.svg';
+import ChocolateQuente from '../assets/img/Type=ChocolateQuente.svg';
+import Cubano from '../assets/img/Type=Cubano.svg';
+import ExpressoCremoso from '../assets/img/Type=ExpressoCremoso.svg';
+import Expresso from '../assets/img/Type=Expresso.svg';
+import Havaiano from '../assets/img/Type=Havaiano.svg';
+import Irlandes from '../assets/img/Type=Irlandês.svg';
+import Latte from '../assets/img/Type=Latte.svg';
+import Macchiato from '../assets/img/Type=Macchiato.svg';
+import Mochaccino from '../assets/img/Type=Mochaccino.svg';
+
+export const coffeeImages = {
+  Americano,
+  Arabe,
+  CafeComLeite,
+  CafeGelado,
+  Capuccino,
+  ChocolateQuente,
+  Cubano,
+  ExpressoCremoso,
+  Expresso,
+  Havaiano,
+  Irlandes,
+  Latte,
+  Macchiato,
+  Mochaccino,
+} as const
