dd5d8c3d1a6886e7bdb88b0466ccb25390e01fe7
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-16 09:37:35 -0300

    * Limpeza no styled do CoffeeCard

--- src/components/CoffeeCard/styles.ts
+++ src/components/CoffeeCard/styles.ts
@@ -46,26 +46,3 @@ export const AddCart = styled.div`
   display: flex;
   gap: 0.5rem;
 `;
-
-
-export const CountContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 2.375rem;
-  width: 4.5rem;
-  justify-content: center;
-  align-items: center;
-  gap: 0.6rem;
-  background-color: ${(props) => props.theme['base-button']};
-  border-radius: 0.375rem;
-`;
-
-export const Operation = styled.section``;
-
-export const Result = styled.section`
-  .number {
-    display: flex;
-    flex-direction: row;
-  }
-`;
-
